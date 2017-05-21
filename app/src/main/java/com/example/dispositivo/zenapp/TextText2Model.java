1dd7d03452e261196bd610be851456d6002220b5
WesleySerra <wesley30v@hotmail.com>
2017-05-21 02:11:50 -0300

    Complementar da ultima alteração

---
+++ app/src/main/java/com/example/dispositivo/zenapp/TextText2Model.java
@@ -0,0 +1,26 @@
+package com.example.dispositivo.zenapp;
+
+/**
+ * Created by Wesley Serra on 21/05/2017.
+ */
+
+public class TextText2Model {
+
+    private String text3, text4;
+
+    public TextText2Model(String text3, String text4)
+    {
+        this.text3 = text3;
+        this.text4 = text4;
+    }
+
+    public String getText3()
+    {
+        return text3;
+    }
+
+    public String getText4()
+    {
+        return text4;
+    }
+}
